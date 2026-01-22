a6407ac18e7ab3dae89a88a6b44b3923c08df94f
canalesmejiad <david.kanale@hotmail.es>
2026-01-21 20:41:03 -0800

    cm

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,8 @@
-import { loadHeaderFooter } from "./utils.mjs";
-loadHeaderFooter();
-
-import { getParam } from "./utils.mjs";
+import { loadHeaderFooter, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
+
+loadHeaderFooter();
 
 const productId = getParam("product");
 const dataSource = new ProductData("tents");
