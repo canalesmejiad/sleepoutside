b7deba9efa0271465aaa75d0d9ba39beb0c3500f
canalesmejiad <david.kanale@hotmail.es>
2026-01-23 13:51:36 -0800

    cm

---
+++ src/js/product-listing.js
@@ -0,0 +1,13 @@
+import { getParam, loadHeaderFooter } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+
+loadHeaderFooter();
+
+const category = getParam("category") || "tents";
+
+const dataSource = new ProductData(category);
+const listElement = document.querySelector(".product-list");
+
+const productList = new ProductList(category, dataSource, listElement);
+productList.init();
