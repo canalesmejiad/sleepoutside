6db581b7192b10225f7c4e08470bb8ab02c6fe54
canalesmejiad <david.kanale@hotmail.es>
2026-01-23 15:50:12 -0800

    CM

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -31,10 +31,28 @@ export function renderListWithTemplate(
   position = "afterbegin",
   clear = false
 ) {
-  if (clear) {
-    parentElement.innerHTML = "";
-  }
-
+  if (clear) parentElement.innerHTML = "";
   const htmlStrings = list.map(templateFn).join("");
   parentElement.insertAdjacentHTML(position, htmlStrings);
+}
+
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.innerHTML = template;
+  if (callback) callback(data);
+}
+
+export async function loadTemplate(path) {
+  const res = await fetch(path);
+  return await res.text();
+}
+
+export async function loadHeaderFooter() {
+  const header = await loadTemplate("/partials/header.html");
+  const footer = await loadTemplate("/partials/footer.html");
+
+  const headerEl = qs("#main-header");
+  const footerEl = qs("#main-footer");
+
+  if (headerEl) renderWithTemplate(header, headerEl);
+  if (footerEl) renderWithTemplate(footer, footerEl);
 }
